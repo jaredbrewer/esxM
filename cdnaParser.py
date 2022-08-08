b260aeb2be03a24ef1839ea08d5a0c098d4a089c
Jared Brewer <jaredbrewer@me.com>
2022-08-08 11:32:50 -0400

    Completed the logic for generating the correct final version of the multifasta with the 5' UTR sequences.

--- cdnaParser.py
+++ cdnaParser.py
@@ -1,7 +1,11 @@
 from Bio import SeqIO
 from Bio.SeqIO.FastaIO import SimpleFastaParser
+import os
 
-fasta = open("/Users/jared/Downloads/Mycobacterium_tuberculosis_H37Rv_genes_v4.fasta")
+script_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(script_path)
+
+fasta = open("Mycobacterium_tuberculosis_H37Rv_genes_v4.fasta")
 fastas = {}
 
 for f in SimpleFastaParser(fasta):
@@ -9,6 +13,30 @@ for f in SimpleFastaParser(fasta):
 	seq = f[1]
 	fastas[name] = seq
 
-with open('/Users/jared/Downloads/Mtb_H37Rv.fa', 'w') as cdna:
-	for gene, seq in fastas.items():
+# This code block is good for writing out the intermediate, but is not strictly necessary.
+
+# with open('/Users/jared/Downloads/Mtb_H37Rv.fa', 'w') as cdna:
+# 	for gene, seq in fastas.items():
+# 		cdna.write(gene + "\n" + seq + "\n")
+
+### Everything above is very general in parsing these files for any use, the subsequent lines are to find and replace the esxMJKPW sequences with their corresponding 5' UTR sequences for the purposes of the subsequent alignment. ###
+
+esx = open("esx_5.fa")
+
+esxs = {}
+
+for f in SimpleFastaParser(esx):
+	name = ">" + f[0]
+	seq = f[1]
+	esxs[name] = seq
+
+esx_genes = ["esxM", "esxJ", "esxP", "esxW", "esxK"]
+
+[fastas.pop(key) for key in esx_genes]
+new = fastas | esxs
+
+with open('Mtb_H37Rv.fa', 'w') as cdna:
+	for gene, seq in new.items():
 		cdna.write(gene + "\n" + seq + "\n")
+
+
